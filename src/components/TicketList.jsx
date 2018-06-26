9e054d2f48f2d104cd5302c573d9cec79e5d2a71
Justin Roller <justin@>
2018-06-26 16:32:21 -0700

    added styles and easier functionality of webpack.

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import Ticket from "./Ticket";
 
 function TicketList(){
+  var myStyledComponentStyles = {
+    backgroundColor: '#ecf0f1',
+    fontFamily: 'sans-serif',
+    paddingTop: '50px'
+  }
   return (
+    <div style={myStyledComponentStyles}>
     <Ticket
       location="3A"
       names="Thato and Haley"
       issue="Firebase will not save record!"/>
+    </div>
   );
 }
 
