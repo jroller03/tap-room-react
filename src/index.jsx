1c8199b1c6324c326cf78d1aee12573cffeb86fc
Justin Roller <justin@>
2018-06-26 15:35:06 -0700

    initial commit.

---
+++ src/index.jsx
@@ -0,0 +1,30 @@
+'use strict';
+
+ReactDOM.render(React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Help Queue'
+  ),
+  React.createElement(
+    'h3',
+    null,
+    '3a'
+  ),
+  React.createElement(
+    'h3',
+    null,
+    'Thato and Haley'
+  ),
+  React.createElement(
+    'p',
+    null,
+    React.createElement(
+      'em',
+      null,
+      'Firebase will not save record!'
+    )
+  )
+), document.getElementById('react-app-root'));
