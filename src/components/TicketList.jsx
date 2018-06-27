03024575df4013ab1c199323ff2d113024ae5291
Justin Roller <justin@>
2018-06-27 15:27:55 -0700

    added eslint and more info for help que.

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,19 +1,40 @@
-import React from "react";
-import Ticket from "./Ticket";
+import React from 'react';
+import Ticket from './Ticket';
+
+var masterTicketList = [
+  {
+    names: 'Thato and Haley',
+    location: '3A',
+    issue: 'Firebase won\'t save record. Halp.'
+  },
+  {
+    names: 'Sleater and Kinney',
+    location: '4B',
+    issue: 'Fox image not displaying on page, can only see duck?'
+  },
+  {
+    names: 'Imani & Jacob',
+    location: '9F',
+    issue: 'Donkey picture not displaying on hover in Zoology app. :('
+  }
+];
 
 function TicketList(){
   var myStyledComponentStyles = {
     backgroundColor: '#ecf0f1',
     fontFamily: 'sans-serif',
     paddingTop: '50px'
-  }
+  };
   return (
     <div style={myStyledComponentStyles}>
-    <Ticket
-      location="3A"
-      names="Thato and Haley"
-      issue="Firebase will not save record!"/>
+      <hr/>
+      {masterTicketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue} />
+      )}
     </div>
+
   );
 }
 
