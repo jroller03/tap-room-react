948d3092baae58e7f7cfc1f25f1720e4a5d4c6ac
Justin Roller <justin@>
2018-06-27 16:38:06 -0700

    added code for error messagefor unknown route, and addded image to page.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import cutePuppies from '../assets/images/cutepuppies.jpg';
 
 function Header(){
   return (
-    <h1>Help Queue!</h1>
+    <div>
+      <h1>Help Queue</h1>
+      <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
+      <img src={cutePuppies}/>
+
+    </div>
   );
 }
 
