b0c764a3a7a40cddc3a3b8267ee2d163d4f039ca
Justin Roller <justin@>
2018-06-28 13:36:30 -0700

    added 2 new components and added new ticket control.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import TicketList from './TicketList';
 import Header from './Header';
-import { Switch, Route } from 'react-router-dom';
-import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
-
+import { Switch, Route } from 'react-router-dom';
 
 function App(){
   return (
     <div>
       <Header/>
-
       <Switch>
         <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
+        <Route path='/newticket' component={NewTicketControl} />
         <Route component={Error404} />
       </Switch>
     </div>
